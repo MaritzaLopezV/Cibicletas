1d13a4e31c44a9247daea32b3d9a4380b0b8c815
dlmdv <ironbodhi@gmail.com>
2020-01-23 12:04:34 -0300

    agregar producto plantilla

---
+++ src/components/AgregarProductos.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+class AgregarProductos extends Component {
+  render() {
+    return (
+        <div className="container mb-5">
+        Agregar Productos
+    </div>
+    );
+  }
+}
+
+export default AgregarProductos;
